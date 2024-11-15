92acdd1e746a6e9ae64e1898eba69693932db439
mishrasatyapriya9 <mishrasatyapriya9@gmial.com>
2024-11-15 23:47:15 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -7,24 +7,44 @@ import connectDB from "./Config/db.js";
 import notes from "../Backend/data/notes.js";
 import Noteroute from "../Backend/Routes/Noteroute.js";
 import Authroute from "../Backend/Routes/Authroutes.js";
-import Carroute   from "../Backend/Routes/Carroute.js"
+import Carroute from "../Backend/Routes/Carroute.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
-app.use(cors());
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin:
+//       "http://localhost:5173" || "https://spyne-ai-car-management.vercel.app/", // Replace with your frontend URL
+//     credentials: true, // If you are using cookies or sessions
+//   })
+// );
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://spyne-ai-car-management.vercel.app/",
+];
 app.use(
   cors({
-    origin:
-      "http://localhost:5173" || "https://spyne-ai-car-management.vercel.app/", // Replace with your frontend URL
+    origin: function (origin, callback) {
+      // Check if the origin is allowed or if it's a request from a browser extension (e.g., React DevTools)
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.startsWith("chrome-extension://")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true, // If you are using cookies or sessions
   })
 );
 
-
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));  //we can take data from the urls
-// app.use(express.static("public"));  //for taking files or photos  
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //we can take data from the urls
+// app.use(express.static("public"));  //for taking files or photos
 app.use(cookieParser());
-// app.use(express.static); 
+// app.use(express.static);
 //now we can send json data in req and res
 app.use(morgan("dev"));
 
